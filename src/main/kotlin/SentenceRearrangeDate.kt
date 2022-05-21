ff7ec2269e7bda8c132247fc2923dee4218de850
unknown <3167ls3@naver.com>
2022-05-21 21:49:16 +0900

    Feat: DT 태그 처리 진도 나감. SentenceRearrangeDate.kt

--- src/main/kotlin/SentenceRearrangeDate.kt
+++ src/main/kotlin/SentenceRearrangeDate.kt
@@ -1,7 +1,14 @@
+import scheduleItem.ItemDate
+
 fun main(){
+    // 정규표현식
+    val ymd = Regex("\\D*(\\d*[\\./년\\s])?\\s?(\\d*[\\./월\\s])?\\s?(\\d+)[^-~]?")
+    val extNum = Regex("\\D")
+
     // 지금 커밋한 데이터는 ner_train_data(17)의 문장입니다
-    val originalSentence = "오 오~ KICKSTA-TEK 초청강연 미국의 대표적인 크라우드 펀딩 서비스 킥스타터 JCT 일시 2019. 12. 06. (2) 16:30-17:30 장소 인하대학교 현경홀(구 중강당)"
-    val originalTag = "O O O O O O O O O O O O DT_YEAR DT_MONTH DT_DAY O TI_DURATION O O O O"
+    // 아래 taggedWords가 완성된 시점까지는 학습해서 데이터 받아올 때 필요 없을 수 있는 코드
+    val originalSentence = "건축가 <건축가 명사특강> 2019.8.27 -9.24 어디서 살 것인가 9.3 180* 매주 (화) 16:00 - 18:00 세종시청 4층 여민실 정태인 |"
+    val originalTag = "O O O DT_OTHERS DT_OTHERS O O O DT_OTHERS O DT_OTHERS DT_OTHERS TL_OTHERS O TL_OTHERS O O O O O"
 
     val beforeWords = originalSentence.split(" ")
     val beforeTags = originalTag.split(" ")
@@ -16,16 +23,51 @@ fun main(){
         taggedWords.add(TaggedWord(beforeWords[i], currentTag))
     }
     println(taggedWords)
+    // 여기까지 인공지능 받을 때 달라질 코드
 
-    val dtCollector = StringBuilder()
 
+    // 1. DT태그가 연속해서 출현하면 붙여서 저장
+    val dtCollector:MutableList<StringBuilder> = mutableListOf()
+    var temp = StringBuilder()
     for (item in taggedWords) {
         val currentTag = item.tag
-        if (!isTagDT(currentTag)) continue
-        dtCollector.append(item.word)
+        if (!isTagDT(currentTag)) {
+            if(temp.isNotEmpty()) dtCollector.add(temp)
+            temp = StringBuilder()
+            continue
+        }
+        else temp.append(item.word)
     }
 
-    println(dtCollector.toString())
+    // 2. DT 처리 결과를 ItemDate에 넘겨줌(텍스트가 출현한 인덱스는 아직 고려하지 않았음)
+    for(i in dtCollector){
+        println("i : $i")
+
+        var resultRegex = ymd.find(i)
+
+        while(resultRegex != null) {
+            val itemDate = ItemDate()
+            var (num1, num2, num3) = resultRegex.destructured
+            num1 = num1.replace(extNum, "")
+            num2 = num2.replace(extNum, "")
+            num3 = num3.replace(extNum, "")
+            println("num1 = $num1, num2 = $num2, num3 = $num3")
+            itemDate.day = num3.toInt()
+
+            if (num1 != "" && num2 == "") itemDate.month = num1.toInt()
+            else itemDate.month = num2.toIntOrNull()
+
+            if (num1 != "" && num2 != "") itemDate.year = num1.toIntOrNull()
+
+            itemDate.range = resultRegex.range
+            println(itemDate)
+
+            resultRegex = resultRegex.next()
+        }
+
+    }
+
+
 }
 
 fun convertTagStringToInt(s: String): Int {
---
+++ src/main/kotlin/tools/StringPositionRecorder.kt
@@ -0,0 +1,4 @@
+package tools
+
+// 구상 도중 만들었지만 안 쓰일 수도 있음
+data class StringPositionRecorder (val str:StringBuilder = java.lang.StringBuilder(), val startIndex:Int = -1)
